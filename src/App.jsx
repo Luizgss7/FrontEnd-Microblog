ffdc54a86af8002b628e4a84df235e88c90676a3
Luiz Guilherme <luizsilvags07@gmail.com>
2026-09-23 22:06:24 -0300

    Adicionando biblioteca MUI Material

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Timeline from './views/Timeline';
+import ButtonUsage from './views/Teste';
 
 function App() {
   return (
@@ -7,11 +8,13 @@ function App() {
       {/* Navigation */}
       <nav>
         <Link to="/">Timeline</Link> |{" "}
+        <Link to="/teste">Teste</Link> |{" "}
       </nav>
 
       {/* Routes */}
       <Routes>
         <Route path="/" element={<Timeline/>} />
+        <Route path="/teste" element={<ButtonUsage/>} />
       </Routes>
     </BrowserRouter>
   );
